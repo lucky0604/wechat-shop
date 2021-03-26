30ff8fa7939fa6d072e5784d63cb5a8183f551c0
Lucky <lucky_soft@163.com>
2021-03-26 14:50:06 +0800

    feature(project): add lxml lib

--- scraper/app/spiders/shop.py
+++ scraper/app/spiders/shop.py
@@ -9,19 +9,26 @@
 import scrapy
 from items import IndexItem
 from scrapy_redis.spiders import RedisSpider
+from lxml import etree
+from chardet import detect
 
 class ShopSpider(scrapy.Spider):
     name = "shop_spider"
     start_urls = [
-        'http://quotes.toscrape.com/tag/humor/',
+        'http://www.bao66.cn/web/',
     ]
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
+        myparser = etree.HTMLParser(encoding="utf-8")
+        tree_node = etree.HTML(response.body, parser=myparser)
+        good_list = tree_node.xpath('//li[@class="f-product inline-block"]')
+        print(good_list, ' ------ print body ----')
+        for item in good_list:
             shop_item = IndexItem()
-            shop_item["item_desc"] = quote.css('span.text::text').get()
+            shop_item["item_desc"] = item.xpath('.//img/@src')
             yield shop_item
-
-        next_page = response.css('li.next a::attr("href")').get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+        # for item in response.css('li.f-product').getall():
+        #     shop_item = IndexItem()
+        #     # shop_item["item_desc"] = item.css('img.').get()
+        #     shop_item["item_desc"] = item
+        #     yield shop_item
